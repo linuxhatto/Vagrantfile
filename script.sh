167191ef387ed870ae8d07c06355603910cc615b
Linuxhatto <fllavrador@hotmail.com>
2017-06-05 14:03:10 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,12 @@
 mkdir -p ~/chef-repo/.chef
+cat <<EOK > ~/chef-repo/.chef/knife.rb
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "fabio"
+client_key               "#{current_dir}/fabio.pem"
+chef_server_url          "https://chef-master/organizations/level3"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+EOK
