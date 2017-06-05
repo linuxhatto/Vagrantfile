b4d13f3baee3f103397f9f8269e02a386014ea8b
Linuxhatto <fllavrador@hotmail.com>
2017-06-05 14:50:53 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ apt-get install git -y
 cd ~
 git clone https://github.com/linuxhatto/Vagrantfile.git
 mkdir -p ~/chef-repo/.chef
-cp ~/chef-repo/fabio.pem ~/chef-repo/.chef
+cp ~/Vagrantfile/chef-repo/fabio.pem ~/chef-repo/.chef
 cat <<EOK > ~/chef-repo/.chef/knife.rb
 # See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
 
